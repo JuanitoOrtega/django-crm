c71ca2c67c946f69f7fecd1d879a303ade92c8bd
Juanito Ortega <ortegaj83@gmail.com>
2022-10-11 23:38:05 -0400

    detail view

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,24 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 
-def home_page(request):
-  # return HttpResponse('Hello, world!')
-  return render(request, 'home.html')
+def lead_list(request):
+  leads = Lead.objects.all()
+  context = {
+    'leads': leads
+  }
+  return render(request, 'leads/lead_list.html', context)
+
+
+def lead_detail(request, pk):
+  lead = Lead.objects.get(id=pk)
+  context = {
+    'lead': lead
+  }
+  return render(request, 'leads/lead_detail.html', context)
+
+
+def lead_create(request):
+  return render(request, 'leads/lead_create.html')
